35167e6f564c133b51dab7d55e63f28171041492
qingcheng <qingcheng@staruniongame.com>
2023-09-01 16:25:25 +0800

    1

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func main() {
+func t() {
 	http.HandleFunc("/v1/chat/completions", HttpPost)
 
 	fmt.Println("Server listening on port 8089...")
@@ -82,6 +82,64 @@ func HttpPost(w http.ResponseWriter, r *http.Request) {
 		fmt.Print(string(body))
 		fmt.Fprintf(w, string(body))
 		w.(http.Flusher).Flush()
+	}
+	return
+}
+
+func main() {
+	api := "https://api.openai.com/v1/chat/completions"
+	str := `{
+	   "model": "gpt-3.5-turbo",
+	   "messages": [
+	
+	       {
+	           "role": "user",
+	           "content": "你知道劲舞团这个游戏吗？"
+	       },{
+	           "role": "user",
+	           "content": "那么怎么玩好这个游戏呢？"
+	       }
+	   ],
+	   "top_p":0.8,
+	   "stream":true
+	}`
+	data := make(map[string]interface{})
+	_ = json.Unmarshal([]byte(str), &data)
+	dataByte, err := json.Marshal(data)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	postData := bytes.NewBuffer(dataByte)
+	var resp *http.Response
+	req, err := http.NewRequest(http.MethodPost, api, postData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer 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")
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println(resp.StatusCode)
+		return
+	}
+	for {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("读取HTTP响应失败：", err)
+			return
+		}
+		if len(body) == 0 {
+			break
+		}
+		fmt.Print(string(body))
 	}
 	return
 }
