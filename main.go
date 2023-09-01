be3b0f995283256b3aeb73013290072f7a5e670d
qingcheng <qingcheng@staruniongame.com>
2023-09-01 16:40:49 +0800

    1

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"ai-api/utils"
 	"log"
 )
 
 func main() {
-	gpt := NewChatGptTool("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")
-	message := []Gpt3Dot5Message{
+	gpt := utils.NewChatGptTool("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")
+	message := []utils.Gpt3Dot5Message{
 		{
 			Role:    "system",
 			Content: "你是一个精通开发的资深工程师，熟悉全栈技术，任何问题都难不倒你",
